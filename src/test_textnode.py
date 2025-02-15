e6fad94bef570ba7eeb21db88cb65ff753669d4f
WoollyBeardBear <peterakluth@gmail.com>
2025-02-15 08:08:44 -0700

    markdown to blocks

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import *
+from markdown import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -77,6 +78,15 @@ class TestTextNode(unittest.TestCase):
         print("text to text node!")
         text = "This is **bold text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         print(text_to_text_nodes(text))
+
+    def test_markdown_to_blocks(self):
+        markdown = (
+            "# This is a heading\n\nThis is a paragraph of text. It has some **bold** and *italic* words inside of it.\n\n* This is the first list item in a list block\n* This is a list item\n* This is another list item"
+        )
+        print("--------------------------")
+        print("Markdown to Blocks")
+        print(markdown_to_blocks(markdown))
+
 
 
 
